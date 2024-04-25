fc03c145458b35a071c249c5e52eafd342c249cc
Enmanuel <enmanuellagares@gmail.com>
2024-04-25 01:31:39 -0400

    change homepage spacing

--- src/renderer/src/pages/home/home.tsx
+++ src/renderer/src/pages/home/home.tsx
@@ -12,9 +12,9 @@ export default function Home(): JSX.Element {
 
 function HomeHeader(): JSX.Element {
   return (
-    <div className="mb-7 flex justify-between px-2">
-      <h1 className="relative font-sans text-3xl font-bold">
-        Music Box <span className="relative -top-5 right-9 text-base text-yellow-500">Pro</span>
+    <div className="mb-8 flex justify-between px-2">
+      <h1 className="relative font-sans text-4xl font-bold">
+        Music Box <span className="relative -top-6 right-10 text-lg text-yellow-500">Pro</span>
       </h1>
 
       <Button>
@@ -26,18 +26,19 @@ function HomeHeader(): JSX.Element {
 
 function Tabs(): JSX.Element {
   const tabs = [
-    { name: 'Songs', path: '/songs' },
+    { name: 'Genres', path: '/genres' },
+    { name: 'Artists', path: '/artists' },
     { name: 'Albums', path: '/albums' },
-    { name: 'Artists', path: '/artists' }
+    { name: 'Songs', path: '/songs' }
   ]
 
   return (
-    <div className="relative flex items-center justify-around">
+    <div className="relative flex items-center justify-between">
       {tabs.map(({ name, path }, index) => (
         <a
           key={index}
           href={path}
-          className="relative font-sans text-lg font-semibold text-gray-500 hover:text-gray-800"
+          className="relative font-sans text-lg font-semibold text-gray-400 hover:text-gray-800"
         >
           {name}
         </a>
