22708474434b32fcd79657dccef0c55f99daa533
Enmanuel <enmanuellagares@gmail.com>
2024-04-29 23:07:28 -0400

    update populares.tsx

--- src/renderer/src/pages/home/populares.tsx
+++ src/renderer/src/pages/home/populares.tsx
@@ -15,15 +15,21 @@ export default Populares
 
 function TopAlbums(): JSX.Element {
   const [scrollValue, setScrollValue] = useState(0)
-  const [enableLeftButton, setEnableLeftButton] = useState(false)
 
   const arr = Array.from({ length: 8 })
+  const childWidthWithGap = 618 / 4
 
-  console.log(scrollValue)
+  const enableLeftButton = scrollValue > 0
+  const enableRightButton = scrollValue < childWidthWithGap * (arr.length - 3)
+
+  console.log('scrollValue: ', scrollValue)
+  console.log('childWithGap * arr.length: ', childWidthWithGap * arr.length)
+  console.log('enableLeftButton', enableLeftButton)
+  console.log('enableRightButton', enableRightButton)
 
   const handleRightClick = (): void => {
     setScrollValue((prev) => {
-      if (prev < childWidthWithGap * (arr.length - 3)) {
+      if (prev < childWidthWithGap * (arr.length - 4)) {
         return prev + childWidthWithGap
       }
       return prev
@@ -33,30 +39,35 @@ function TopAlbums(): JSX.Element {
   const handleLeftClick = (): void => {
     setScrollValue((prev) => {
       if (prev > 0) {
-        setEnableLeftButton((prev) => !prev)
         return prev - childWidthWithGap
       }
       return prev
     })
   }
 
-  const childWidthWithGap = 618 / 4 + 20
-
   return (
-    <div className="flex flex-col justify-center gap-4">
-      <h3 className="w-full font-sans text-3xl font-bold">Albumes Populares</h3>
+    <div className={`flex flex-col justify-center`}>
+      <h3 className="mb-4 w-full font-sans text-3xl font-bold">Albumes Populares</h3>
 
       <div className="flex items-center justify-between">
         <Button>
-          <ChevronLeft onClick={handleLeftClick} size={32} className="shrink-0 cursor-pointer" />
+          <ChevronLeft
+            onClick={handleLeftClick}
+            size={32}
+            className={`shrink-0 cursor-pointer ${!enableLeftButton && 'text-gray-400'}`}
+          />
         </Button>
-        <div className="flex gap-5 overflow-hidden">
+        <div className="flex overflow-hidden">
           {arr.map((_, index) => (
             <Album key={index} translateValue={scrollValue} />
           ))}
         </div>
         <Button>
-          <ChevronRight onClick={handleRightClick} size={32} className="shrink-0 cursor-pointer" />
+          <ChevronRight
+            onClick={handleRightClick}
+            size={32}
+            className={` shrink-0 cursor-pointer ${!enableRightButton && 'text-gray-400'}`}
+          />
         </Button>
       </div>
     </div>
@@ -67,19 +78,22 @@ function Album({ translateValue }: { translateValue?: number }): JSX.Element {
   return (
     <div
       style={{ transform: `translateX(-${translateValue}px` }}
-      className="shrink-0 grow-0 basis-1/4 translate-x-5 cursor-pointer overflow-hidden rounded-md duration-500"
+      className="shrink-0 grow-0 basis-1/4 translate-x-5 cursor-pointer overflow-hidden rounded-md px-4 duration-500"
     >
-      <img src="https://upload.wikimedia.org/wikipedia/en/6/60/Bad_Bunny_-_Un_Verano_Sin_Ti.png"></img>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/6/60/Bad_Bunny_-_Un_Verano_Sin_Ti.png"
+        className="rounded-md"
+      ></img>
     </div>
   )
 }
 
 function TopSongs(): JSX.Element {
   return (
-    <div className="font-sans text-3xl font-bold">
+    <div className="flex flex-col font-sans text-3xl font-bold">
       <h3 className="mb-4">Canciones Populares</h3>
-      <div className="scrollbar flex h-[400px] flex-col overflow-y-auto px-3">
-        {Array.from({ length: 5 }).map((_, index) => (
+      <div className="scrollbar flex h-[450px] flex-col overflow-y-auto px-3">
+        {Array.from({ length: 8 }).map((_, index) => (
           <Song key={index} />
         ))}
       </div>
